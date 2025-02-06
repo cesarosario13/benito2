a850cd4eee205ee5f867bd33711c76dae8a47df6
Carlos Rodriguez <rodriguezlinaresca@uvm.edu.ve>
2025-02-06 18:47:12 -0400

    Modificación del archivo app.js

--- socialnetwork/app.js
+++ socialnetwork/app.js
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// Configuración de EJS como motor de plantillas
 app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware para parsear el cuerpo de las solicitudes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Rutas
+const userRoutes = require('./routes/users');
+const postRoutes = require('./routes/posts');
+const friendshipRoutes = require('./routes/friendships');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+app.use('/friendships', friendshipRoutes);
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-module.exports = app;
