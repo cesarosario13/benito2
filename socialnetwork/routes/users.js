617019b0d20904f2f02e3dd3a8abdeaf00b62f15
Nelpena <nelpe611@gmail.com>
2025-02-06 22:22:56 -0400

    Agregadas las Implementacion de Rutas de User.js

--- socialnetwork/routes/users.js
+++ socialnetwork/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+
+router.post('/', UserController.addUser);
+router.put('/:id', UserController.editUser);
+router.delete('/:id', UserController.deleteUser);
+router.get('/:id/posts', UserController.getUserPosts);
 
 module.exports = router;
