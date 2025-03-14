32f9c2b7cc1b26a1bbd31eadbc92752052309ef6
Nelpena <nelpe611@gmail.com>
2025-03-14 04:44:03 -0400

    Agregado Separacion de Intereses para Friendship/Routes

--- socialnetwork/routes/friendships.js
+++ socialnetwork/routes/friendships.js
@@ -2,8 +2,37 @@ const express = require('express');
 const router = express.Router();
 const FriendshipController = require('../controllers/FriendshipController');
 
-router.post('/', FriendshipController.addFriendship); 
-router.delete('/:userId1/:userId2', FriendshipController.deleteFriendship); 
-router.get('/:userId/feed', FriendshipController.getFeed); 
+
+router.post('/', function (req, res, next) {
+    FriendshipController.addFriendship(req)
+        .then((nuevaAmistad) => {
+            res.status(201).send(nuevaAmistad); 
+        })
+        .catch((error) => {
+            res.status(400).json({ message: error.message }); 
+        });
+});
+
+
+router.delete('/:userId1/:userId2', function (req, res, next) {
+    FriendshipController.deleteFriendship(req)
+        .then(() => {
+            res.status(204).send(); 
+        })
+        .catch((error) => {
+            res.status(400).json({ message: error.message });
+        });
+});
+
+
+router.get('/:userId/feed', function (req, res, next) {
+    FriendshipController.getFeed(req)
+        .then((feed) => {
+            res.status(200).send(feed); 
+        })
+        .catch((error) => {
+            res.status(500).json({ message: error.message }); 
+        });
+});
 
 module.exports = router;
