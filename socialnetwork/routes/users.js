b21e6739cf6d6f0b85fbcc805a3e467ede67801e
Nelpena <nelpe611@gmail.com>
2025-03-14 02:23:41 -0400

    Agregado Separacion de Intereses en Users/Routes

--- socialnetwork/routes/users.js
+++ socialnetwork/routes/users.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/UserController');
-
-router.get('/', UserController.getUsers);
-
-router.post('/', UserController.addUser);
-
+const express = require('express'); 
+const router = express.Router(); 
+const UserController = require('../controllers/UserController'); 
+ 
+ 
+router.get('/', function (req, res, next) { 
+    UserController.getUsers() 
+        .then((usuarios) => { 
+            res.status(200).send(usuarios);  
+        }) 
+        .catch((error) => { 
+            res.status(500).render("error", { message: error.message });  
+        }); 
+}); 
+ 
+ 
+router.post('/', function (req, res, next) { 
+    UserController.addUser(req)  
+        .then((nuevoUsuario) => { 
+            res.status(201).send(nuevoUsuario); 
+        }) 
+        .catch((error) => { 
+            res.status(400).render("error", { message: error.message });  
+        }); 
+}); 
+ 
 module.exports = router;
